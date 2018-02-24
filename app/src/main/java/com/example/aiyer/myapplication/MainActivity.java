a65a9b2ab74dacd89eb4be1fd6b53c89bddba80b
pranovic24 <pranay.sd@gmail.com>
2018-02-24 17:30:14 +0800

    Personal Details Part 2
    Button, Change in Orientation, Phone.
    TODO:
    Upload Image

--- app/src/main/java/com/example/aiyer/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aiyer/myapplication/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.aiyer.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
 
         startActivityForResult(Intent.createChooser(intent, "Select Image From Gallery"), 1);
-
-    }
 }
 
+}
