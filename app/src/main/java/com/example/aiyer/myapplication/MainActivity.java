d778d9a6a06b2f58aa1075888ef3c148296f8eb2
pranovic24 <pranay.sd@gmail.com>
2018-02-24 14:27:43 +0800

    personal details

---
+++ app/src/main/java/com/example/aiyer/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.aiyer.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+
+        Intent intent = new Intent();
+
+        intent.setType("image/*");
+
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent, "Select Image From Gallery"), 1);
+
+    }
+}
+
